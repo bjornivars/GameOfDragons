958d9d4ebbff2e193858cf6caaf2cb1281cb4073
Bjørn-Ivar Skuggen <bjorn-ivar.skuggen@hotmail.com>
2019-11-18 00:36:06 +0100

    Select characters and change of active player in index page

--- js/script.js
+++ js/script.js
@@ -3,9 +3,86 @@
 
 
 let apiUrl = 'https://anapioficeandfire.com/api/characters/';
-let player1 = "";
-let player2 = "";
+let player1Character = "";
+let player2Character = "";
+let player1Active = true;
+
+function activeClass(){
+if(player1Active){
+    document.getElementById("player1Name").classList.add("active");
+    document.getElementById("player2Name").classList.remove("active");
+    document.getElementById("player2Name").classList.add("disabled");
+
+}else{
+    document.getElementById("player2Name").classList.add("active");
+    document.getElementById("player1Name").classList.remove("active");
+    document.getElementById("player1Name").classList.add("disabled");
+    }
+}
+activeClass();
+
+function setCorrectPlayerUrl(characterId){
+    if(player1Active){
+        player1Character = apiUrl + characterId;
+        console.log("player1 " + player1Character);
+    }else{
+        player2Character = apiUrl + characterId;
+        console.log("player2 " + player2Character);
+    }
+}
+
+function choosePlayer(clicked){
+    player1Character = clicked; 
+    switch(player1Character){
+        case "selectJon":
+            setCorrectPlayerUrl("583");
+          break;
+          case "selectSansa":
+            setCorrectPlayerUrl("957");
+          break;
+          case "selectEddard":
+            setCorrectPlayerUrl("339");
+          break;
+          case "selectArya":
+            setCorrectPlayerUrl("148");
+          break;
+          case "selectDaenerys":
+            setCorrectPlayerUrl("238");
+          break;
+          case "selectCersei":
+            setCorrectPlayerUrl("238");
+          break;
+          case "selectJaime":
+            setCorrectPlayerUrl("529");
+          break;
+          case "selectJoffrey":
+            setCorrectPlayerUrl("565");
+          break;
+          case "selectBrandon":
+            setCorrectPlayerUrl("206");
+          break;
+          case "selectTyrion":
+            setCorrectPlayerUrl("1052");
+          break;
     
+        default:
+        player1Character = "";
+      }
+
+}
+function readyUp(){
+    if(player1Character != "" && player1Active){
+      player1Active = false;
+        console.log("character 1 = " + player1Character);
+        activeClass();
+    }else{
+        player1Active = true;
+        console.log("character 2 = " + player1Character);
+        activeClass();
+    }
+  }
+
+
 
     var card = document.getElementById("cardContainer"); 
     var cardText = document.getElementById("buttonId"); 
