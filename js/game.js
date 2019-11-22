a66d8f0a3f70da8636bc2eea689b23e972f139fe
Bjørn-Ivar Skuggen <bjorn-ivar.skuggen@hotmail.com>
2019-11-22 02:45:51 +0100

    player tiles moving on dice click

--- js/game.js
+++ js/game.js
@@ -1,20 +1,28 @@
 // some variables
-let dice, diceRoll, scores, activePlayer1, player1Character, player2Character;
-
-player1Character = document.getElementById("player1Character");
-player2Character = document.getElementById("player2Character");
-dice = Math.floor(Math.random() * 6) + 1;
+let dice, diceRoll, scores, activePlayer, player1Character, player2Character;
+scores = [0, 0];
+activePlayer = 0;
+roundScore = 0;
+//player1Character = document.getElementById("player1Character");
+//player2Character = document.getElementById("player2Character");
 gamePlaying = true;
+player1Score = 0;
+player2Score = 0;
 
 let player1 = sessionStorage.getItem('player1');
 let player2 = sessionStorage.getItem('player2');
-
+ player1Character = document.getElementById("player1img");
+ player2Character = document.getElementById("player2img");
 
 function setPlayerImg(){
-    let player1Character = document.getElementById("player1img");
+
     player1Character.src = "graphics/img/icons/" + player1 + ".png";
-    let player2Character = document.getElementById("player2img");
     player2Character.src = "graphics/img/icons/" + player2 + ".png";
+    
+//document.querySelector("#square-"+"12").appendChild(player1Character);
+
+
+
     }
 setPlayerImg();
 
@@ -40,20 +48,69 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
             diceDOM.src = 'graphics/img/dice/dice-' + dice + '.png'; 
         }, 250);
         // console.log(dice);
-    } 
+
+        if (dice) {
+            //Add score
+            roundScore = dice;
+           // console.log("roundscore = " + roundScore);
+            scores[activePlayer] += roundScore;
+           // console.log(scores[activePlayer]);
+            console.log("player 1: " + scores[0]);
+            console.log("player 2: " + scores[1]);
+
+            if(scores[0] == scores[activePlayer]){
+                setPlayerImg();
+               let x = scores[0].toString();
+               console.log(x);
+           let y = document.getElementById(x);
+           console.log(y);
+          // y.appendChild(player1Character);
+          document.getElementById(x).appendChild(player1Character);
+          nextPlayer();
+            }else{
+                console.log("player2 turn");
+                setPlayerImg();
+                let x = scores[1].toString();
+                let y = document.getElementById(x);
+                console.log(y);
+               document.getElementById(x).appendChild(player2Character);
+               nextPlayer(); 
+               
+            }
+
+          //  document.querySelector("#square-" + scores[activePlayer]).appendChild(player1Character);
+
+
+        } 
+
+
+
+
+    }    
+
+         
+    // player1Place.appendChild(player1Character);
 
 });
 
 } game();
 function nextPlayer() {
     //Next player
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    if(activePlayer === 0){
+        activePlayer = 1;
+    }else{
+        activePlayer = 0;
+
+
+       // activePlayer = player2Character;
+    }
+
+ //   activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
 
-    document.getElementById('.player1Character').classList.toggle('active');
-    document.getElementById('.player2Character').classList.toggle('active');
+ //   document.getElementById('.player1Character').classList.toggle('active');
+   // document.getElementById('.player2Character').classList.toggle('active');
 }
 
 // The initial state of the game before starting to play
 
-document.getElementById("square-29").appendChild(player1Character);
